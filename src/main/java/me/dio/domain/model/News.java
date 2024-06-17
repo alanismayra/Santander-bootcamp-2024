d3755eebcc20eede79c452c8425b18354f442a70
alanismayra <alanismayra5000@gmail.com>
2024-06-17 20:19:47 -0300

    publicação no railway

--- src/main/java/me/dio/domain/model/News.java
+++ src/main/java/me/dio/domain/model/News.java
@@ -2,7 +2,7 @@ package me.dio.domain.model;
 
 import jakarta.persistence.Entity;
 
-@Entity(name = "tb_feature")
-public class Feature extends BaseItem {
+@Entity(name = "tb_news")
+public class News extends BaseItem {
 
 }
